18a9449fb1b666e5bc656b420fcd5e79da821d2c
WilliamPM <william_parcianello@hotmail.com>
2015-06-15 12:29:03 -0300

    Commit

---
+++ EvaluationSystem/EvaluationSystemUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EvaluationSystemUI.Startup))]
+namespace EvaluationSystemUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
