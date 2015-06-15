28c024805c339588ef0624729462ee47d924abba
WilliamPM <william_parcianello@hotmail.com>
2015-06-15 20:17:20 -0300

    First Commit

--- EvaluationSystem/EvaluationSystemBLL/Properties/AssemblyInfo.cs
+++ EvaluationSystem/EvaluationSystemBLL/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6af3b164-5211-45cf-b5ed-7337c512dcbb")]
+[assembly: Guid("570178b3-b33c-48fd-ba1f-7ba69c45388f")]
 
 // Version information for an assembly consists of the following four values:
 //
