41e9a2dbff79eca5fcbba35d99899c1420dc321c
WilliamPM <william_parcianello@hotmail.com>
2015-06-16 18:43:54 -0300

    Camada DAL

---
+++ EvaluationSystem/EvaluationSystemDAL/DBClasses/UsersDAL.cs
@@ -0,0 +1,21 @@
+﻿using EvaluationSystemDAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationSystemDAL.DBClasses
+{
+    class UsersDAL : IUsersDAL
+    {
+        public users CreateNewUser(users NewUser, String stringConexao)
+        {
+            
+            users.add();
+
+            
+            return User;
+        }
+    }
+}
---
+++ EvaluationSystem/EvaluationSystemDAL/DBClasses/UsersEvaluationQuestionDAL.cs
@@ -0,0 +1,19 @@
+﻿using EvaluationSystemDAL.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationSystemDAL.DBClasses
+{
+    class UsersEvaluationQuestionDAL : IUsersEvaluationQuestionDAL
+    {
+        public List<IEnumerable> GetStudentGrade(users us, evaluation ev)
+        {
+            List<IEnumerable> List = null;
+            return List;
+        }
+    }
+}
