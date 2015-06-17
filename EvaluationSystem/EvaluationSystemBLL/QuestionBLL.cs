1578906e9af2dbb63e8b60c775097c86175ab36a
WilliamPM <william_parcianello@hotmail.com>
2015-06-17 18:02:39 -0300

    Commit 1.1

---
+++ EvaluationSystem/EvaluationSystemBLL/QuestionBLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationSystemBLL
+{
+    public class QuestionBLL
+    {
+        public Boolean create(EvaluationSystemDAL.question question)
+        {
+            if ((question.AnswerA =="") || (question.AnswerB =="") ||  (question.AnswerC=="") || (question.AnswerD =="") || 
+                (question.AnswerE =="") || (question.category == null) || (question.commentary ==null) ||
+                (question.CorrectAnswer == "") || (question.Enunciation == ""))
+            {                
+                return false;
+            }
+
+            return true;
+        } 
+    }
+}
